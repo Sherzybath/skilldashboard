2f09d156ab10564e9f727b86a33cd99b8da5aa50
Sherzybath <sdzyrsmy@gmail.com>
2024-08-29 01:30:30 +0530

    Pookie

--- src/Components/TagList.js
+++ src/Components/TagList.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell, faCircleQuestion, faMessage} from '@fortawesome/free-regular-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const TagList = () => {
   return (
-    <div>TagList</div>
+    <div className='TagList'>
+      <div className='ManualContainer'>
+        <div className='ManualSearch'>
+        <FontAwesomeIcon icon={faMagnifyingGlass} className='icons'/>
+        <input placeholder='Search'></input>
+        </div>
+      </div>
+      <span>Jobs</span>
+      <div className='line'></div>
+
+    </div>
   )
 }
 
